7aefbb34f7b46288ea18e48b1da8b278579cafc1
liujiaxian <liujiaxioo@163.com>
2017-03-30 18:07:14 +0800

    first commit

---
+++ MvcApi/Controllers/ValuesController.cs
@@ -0,0 +1,593 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Model;
+
+namespace MvcApi.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        bds252114683_dbEntities db = new bds252114683_dbEntities();
+
+        int count = 0;
+        int pageSize = 5;
+
+        #region 博客接口
+        #region 首页博客信息
+        public object GetBlog()
+        {
+            var list = db.person_vyw_blog_type.Where(c => true).OrderByDescending(c => c.blogID).Take(5).ToList();
+
+            var newlist = from d in list
+                          select new
+                          {
+                              userPictureUrl = d.userPictureUrl,
+                              blogID = d.blogID,
+                              nickName = string.IsNullOrEmpty(d.nickName) ? d.username : d.nickName,
+                              title = d.title,
+                              typeID = d.typeID,
+                              typeName = d.typeName,
+                              userID = d.userID,
+                              hot = d.hot,
+                              addTime = Convert.ToDateTime(d.addTime).ToString("yyyy-MM-dd HH:mm:ss"),
+                          };
+
+            return newlist;
+        }
+        #endregion
+
+        #region 博客列表
+        [HttpPost]
+        public object PostBlogList(int? page, int? typeid)
+        {
+            if (string.IsNullOrEmpty(page.ToString()))
+            {
+                return null;
+            }
+            IQueryable<person_vyw_blog_type> result = db.person_vyw_blog_type.Where(c => true);
+            List<person_vyw_blog_type> list = null;
+            object newlist = null;
+
+            if (string.IsNullOrEmpty(typeid.ToString()))
+            {
+                typeid = 0;
+            }
+
+
+            if (typeid > 0)
+            {
+                result = result.Where(c => c.typeID == typeid);
+            }
+
+
+            count = result.Count();
+
+            int pageCount = (int)Math.Ceiling(count * 1.0 / pageSize);
+
+            if (page < 1)
+            {
+                return null;
+            }
+
+            if (page > pageCount)
+            {
+                return null;
+            }
+
+            if (count > 0)
+            {
+                list = result.OrderByDescending(c => c.blogID).Skip((Convert.ToInt32(page) - 1) * pageSize).Take(pageSize).ToList();
+
+                newlist = from d in list
+                          select new
+                          {
+                              blogID = d.blogID,
+                              content = d.content,
+                              describe = d.describe,
+                              nickName = string.IsNullOrEmpty(d.nickName) ? d.username : d.nickName,
+                              hot = d.hot,
+                              tags = d.tags,
+                              title = d.title,
+                              typeName = d.typeName,
+                              typeID = d.typeID,
+                              userID = d.userID,
+                              userPictureUrl = d.userPictureUrl,
+                              addTime = Convert.ToDateTime(d.addTime).ToString("yyyy-MM-dd HH:mm:ss")
+                          };
+            }
+
+            return newlist;
+        }
+        #endregion
+
+        #region 博客详细
+        public object GetBlogDetail(int? id)
+        {
+            if (string.IsNullOrEmpty(id.ToString()))
+            {
+                return null;
+            }
+
+            var model = db.person_vyw_blog_type.Where(c => c.blogID == id).FirstOrDefault();
+            if (model == null)
+            {
+                return null;
+            }
+
+            return model;
+        }
+        #endregion
+        #endregion
+
+        #region 轮播接口
+        #region 首页轮播信息
+        public object GetBanner()
+        {
+            var list = db.person_vyw_banner_user.Where(c => c.isDelete == false && c.isRecommend == true).OrderByDescending(c => c.bannerID).ToList();
+
+            var newlist = from d in list
+                          select new
+                          {
+                              url = d.url,
+                              bannerID = d.bannerID,
+                              title = d.title,
+                              hot = d.hot,
+                              addTime = Convert.ToDateTime(d.addTime).ToString("yyyy-MM-dd HH:mm:ss")
+                          };
+
+            return newlist;
+        }
+        #endregion
+
+        #region 轮播详细
+        public object GetBannerDetail(int? id)
+        {
+            if (string.IsNullOrEmpty(id.ToString()))
+            {
+                return null;
+            }
+
+            var model = db.person_vyw_banner_user.Where(c => c.isDelete == false && c.bannerID == id).FirstOrDefault();
+            if (model == null)
+            {
+                return null;
+            }
+
+            return model;
+        }
+        #endregion
+
+        #region 轮播列表
+        [HttpPost]
+        public object PostBannerList(int? page)
+        {
+            if (string.IsNullOrEmpty(page.ToString()))
+            {
+                return null;
+            }
+            IQueryable<person_vyw_banner_user> result = db.person_vyw_banner_user.Where(c => c.isDelete == false && c.isRecommend == false);
+            List<person_vyw_banner_user> list = null;
+            object newlist = null;
+
+            //switch (orderbyid)
+            //{
+            //    case -1://推荐
+            //        result = result.Where(c => c.isRecommend == true).OrderByDescending(c => c.bannerID);
+            //        break;
+            //    case -2://热门
+            //        result = result.OrderByDescending(c => c.hot);
+            //        break;
+            //    default:
+            //        result = result.OrderByDescending(c => c.bannerID);
+            //        break;
+            //}
+
+            count = result.Count();
+
+            int pageCount = (int)Math.Ceiling(count * 1.0 / pageSize);
+
+            if (page < 1)
+            {
+                return null;
+            }
+
+            if (page > pageCount)
+            {
+                return null;
+            }
+
+            if (count > 0)
+            {
+                list = result.OrderByDescending(c => c.hot).Skip((Convert.ToInt32(page) - 1) * pageSize).Take(pageSize).ToList();
+
+                newlist = from d in list
+                          select new
+                          {
+                              bannerID = d.bannerID,
+                              content = d.content,
+                              describe = d.describe,
+                              nickName = string.IsNullOrEmpty(d.nickName) ? d.username : d.nickName,
+                              hot = d.hot,
+                              tags = d.tags,
+                              title = d.title,
+                              url = d.url,
+                              userID = d.userID,
+                              userPictureUrl = d.userPictureUrl,
+                              addTime = Convert.ToDateTime(d.addTime).ToString("yyyy-MM-dd HH:mm:ss")
+                          };
+            }
+
+            return newlist;
+        }
+        #endregion
+
+        #endregion
+
+        #region 获取会员信息
+        public object GetMemberInfo(int? id)
+        {
+            if (string.IsNullOrEmpty(id.ToString()))
+            {
+                return null;
+            }
+
+            var model = db.person_vyw_user_picture.Where(c => c.status == (int)MvcApi.Models.Enum_Member_Status.正常 && c.userID == id).FirstOrDefault();
+            if (model == null)
+            {
+                return null;
+            }
+
+            var newmodel = new
+            {
+                nickName = string.IsNullOrEmpty(model.nickName) ? model.username : model.nickName,
+                sex = Enum.GetName(typeof(MvcApi.Models.Enum_Sex), model.sex),
+                email = model.email,
+                addTime = Convert.ToDateTime(model.signTime).ToString("yyyy-MM-dd HH:mm:ss"),
+                score = model.score,
+                motto = model.motto,
+                signature = model.signature,
+                url = model.url
+            };
+
+            return newmodel;
+        }
+        #endregion
+
+        #region 计划接口
+        #region 首页计划信息
+        public object GetPlan()
+        {
+            var list = db.person_vyw_plan_user.Where(c => true).OrderByDescending(c => c.planID).Take(5).ToList();
+
+            var newlist = from d in list
+                          select new
+                          {
+                              url = d.url,
+                              blogID = d.planID,
+                              nickName = string.IsNullOrEmpty(d.nickName) ? d.username : d.nickName,
+                              title = d.title,
+                              isRecommend = Convert.ToBoolean(d.isRecommend) ? "推荐" : "未推荐",
+                              userID = d.userID,
+                              hot = d.hot,
+                              addTime = Convert.ToDateTime(d.addTime).ToString("yyyy-MM-dd HH:mm:ss"),
+                          };
+
+            return newlist;
+        }
+        #endregion
+
+        #region 计划列表
+        [HttpPost]
+        public object PostPlanList(int? page)
+        {
+            if (string.IsNullOrEmpty(page.ToString()))
+            {
+                return null;
+            }
+            IQueryable<person_vyw_plan_user> result = db.person_vyw_plan_user.Where(c => true);
+            List<person_vyw_plan_user> list = null;
+            object newlist = null;
+
+
+
+            count = result.Count();
+
+            int pageCount = (int)Math.Ceiling(count * 1.0 / pageSize);
+
+            if (page < 1)
+            {
+                return null;
+            }
+
+            if (page > pageCount)
+            {
+                return null;
+            }
+
+            if (count > 0)
+            {
+                list = result.OrderByDescending(c => c.planID).Skip((Convert.ToInt32(page) - 1) * pageSize).Take(pageSize).ToList();
+
+                newlist = from d in list
+                          select new
+                          {
+                              url = d.url,
+                              blogID = d.planID,
+                              nickName = string.IsNullOrEmpty(d.nickName) ? d.username : d.nickName,
+                              title = d.title,
+                              isRecommend = Convert.ToBoolean(d.isRecommend) ? "推荐" : "未推荐",
+                              userID = d.userID,
+                              hot = d.hot,
+                              addTime = Convert.ToDateTime(d.addTime).ToString("yyyy-MM-dd HH:mm:ss"),
+                          };
+            }
+
+            return newlist;
+        }
+        #endregion
+
+        #region 计划详细
+        public object GetPlanDetail(int? id)
+        {
+            if (string.IsNullOrEmpty(id.ToString()))
+            {
+                return null;
+            }
+
+            var list = db.person_vyw_plan_content.Where(c => c.planID == id).ToList();
+            if (list.Count <= 0)
+            {
+                return null;
+            }
+
+            return list;
+        }
+        #endregion
+        #endregion
+
+        #region 图书接口
+        #region 首页图书信息
+        public object GetBook()
+        {
+            var list = db.person_vyw_reader_user.Where(c => true).OrderByDescending(c => c.readerID).Take(5).ToList();
+
+            var newlist = from d in list
+                          select new
+                          {
+                              url = d.url,
+                              blogID = d.readerID,
+                              nickName = string.IsNullOrEmpty(d.nickName) ? d.username : d.nickName,
+                              title = d.readerTitle,
+                              typeid = d.typeID,
+                              typeName = d.typeName,
+                              userID = d.userID,
+                              hot = d.hot,
+                              addTime = Convert.ToDateTime(d.addTime).ToString("yyyy-MM-dd HH:mm:ss"),
+                          };
+
+            return newlist;
+        }
+        #endregion
+
+        #region 图书列表
+        [HttpPost]
+        public object PostBookList(int? page, int? typeid)
+        {
+            if (string.IsNullOrEmpty(page.ToString()))
+            {
+                return null;
+            }
+            IQueryable<person_vyw_reader_user> result = db.person_vyw_reader_user.Where(c => true);
+            List<person_vyw_reader_user> list = null;
+            object newlist = null;
+
+            if (typeid > 0)
+            {
+                result = result.Where(c => c.typeID == typeid);
+            }
+
+
+            count = result.Count();
+
+            int pageCount = (int)Math.Ceiling(count * 1.0 / pageSize);
+
+            if (page < 1)
+            {
+                return null;
+            }
+
+            if (page > pageCount)
+            {
+                return null;
+            }
+
+            if (count > 0)
+            {
+                list = result.OrderByDescending(c => c.readerID).Skip((Convert.ToInt32(page) - 1) * pageSize).Take(pageSize).ToList();
+
+                newlist = from d in list
+                          select new
+                          {
+                              url = d.url,
+                              blogID = d.readerID,
+                              nickName = string.IsNullOrEmpty(d.nickName) ? d.username : d.nickName,
+                              title = d.readerTitle,
+                              typeid = d.typeID,
+                              typeName = d.typeName,
+                              userID = d.userID,
+                              hot = d.hot,
+                              addTime = Convert.ToDateTime(d.addTime).ToString("yyyy-MM-dd HH:mm:ss"),
+                          };
+            }
+
+            return newlist;
+        }
+        #endregion
+
+        #region 图书详细
+        public object GetBookDetail(int? id)
+        {
+            if (string.IsNullOrEmpty(id.ToString()))
+            {
+                return null;
+            }
+
+            var model = db.person_vyw_reader_user.Where(c => c.readerID == id).FirstOrDefault();
+            if (model == null)
+            {
+                return null;
+            }
+
+            var newmodel = new
+            {
+                title = model.readerTitle,
+                tags = model.tags,
+                typeID = model.typeID,
+                typeName = model.typeName,
+                url = model.url,
+                userID = model.userID,
+                hot = model.hot,
+                readerDescribe = model.readerDescribe,
+                nickName = string.IsNullOrEmpty(model.nickName) ? model.username : model.nickName
+            };
+
+            return newmodel;
+        }
+        #endregion
+        #endregion
+
+        #region 项目接口
+        #region 首页项目信息
+        public object GetCase()
+        {
+            var list = db.person_vyw_case_type_user_picture.Where(c => c.isDelete == false).OrderByDescending(c => c.caseID).Take(5).ToList();
+
+            var newlist = from d in list
+                          select new
+                          {
+                              url = d.url,
+                              userID = d.userID,
+                              userPictureUrl = d.userPictureUrl,
+                              bannerID = d.caseID,
+                              typeName = d.name,
+                              typeID = d.typeID,
+                              title = d.title,
+                              nickName  = string.IsNullOrEmpty(d.nickName)?d.username:d.nickName,
+                              hot = d.hot,
+                              addTime = Convert.ToDateTime(d.addTime).ToString("yyyy-MM-dd HH:mm:ss")
+                          };
+
+            return newlist;
+        }
+        #endregion
+
+        #region 项目详细
+        public object GetCaseDetail(int? id)
+        {
+            if (string.IsNullOrEmpty(id.ToString()))
+            {
+                return null;
+            }
+
+            var model = db.person_vyw_case_type_user_picture.Where(c => c.isDelete == false && c.caseID == id).FirstOrDefault();
+            if (model == null)
+            {
+                return null;
+            }
+
+            var newmodel = new { 
+               title =model.title,
+               describe =model.describe,
+               content =model.content,
+               hot =model.hot,
+               name =model.name,
+               typeID =model.typeID,
+               userID =model.userID,
+               nickName =string.IsNullOrEmpty(model.nickName)?model.username:model.nickName,
+               addTime = Convert.ToDateTime(model.addTime).ToString("yyyy-MM-dd HH:mm:ss")
+            };
+
+            return model;
+        }
+        #endregion
+
+        #region 项目列表
+        [HttpPost]
+        public object PostCaseList(int? page,int? typeid)
+        {
+            if (string.IsNullOrEmpty(page.ToString()))
+            {
+                return null;
+            }
+            IQueryable<person_vyw_case_type_user_picture> result = db.person_vyw_case_type_user_picture.Where(c => c.isDelete == false);
+            List<person_vyw_case_type_user_picture> list = null;
+            object newlist = null;
+
+            //switch (orderbyid)
+            //{
+            //    case -1://推荐
+            //        result = result.Where(c => c.isRecommend == true).OrderByDescending(c => c.bannerID);
+            //        break;
+            //    case -2://热门
+            //        result = result.OrderByDescending(c => c.hot);
+            //        break;
+            //    default:
+            //        result = result.OrderByDescending(c => c.bannerID);
+            //        break;
+            //}
+
+            if (string.IsNullOrEmpty(typeid.ToString()))
+            {
+                typeid = 0;
+            }
+
+            if (typeid>0)
+            {
+                result = result.Where(c=>c.typeID==typeid);
+            }
+
+            count = result.Count();
+
+            int pageCount = (int)Math.Ceiling(count * 1.0 / pageSize);
+
+            if (page < 1)
+            {
+                return null;
+            }
+
+            if (page > pageCount)
+            {
+                return null;
+            }
+
+            if (count > 0)
+            {
+                list = result.OrderByDescending(c => c.caseID).Skip((Convert.ToInt32(page) - 1) * pageSize).Take(pageSize).ToList();
+
+                newlist = from d in list
+                          select new
+                          {
+                              url = d.url,
+                              userID = d.userID,
+                              userPictureUrl = d.userPictureUrl,
+                              bannerID = d.caseID,
+                              typeName = d.name,
+                              typeID = d.typeID,
+                              title = d.title,
+                              nickName = string.IsNullOrEmpty(d.nickName) ? d.username : d.nickName,
+                              hot = d.hot,
+                              addTime = Convert.ToDateTime(d.addTime).ToString("yyyy-MM-dd HH:mm:ss")
+                          };
+            }
+
+            return newlist;
+        }
+        #endregion
+
+        #endregion
+    }
+}
+
---
+++ MvcApi/Models/EnumType.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcApi.Models
+{
+
+    public enum Enum_Return
+    {
+        成功 = 0,
+        失败,
+        登录失效,
+        账号未激活
+    }
+
+    public enum Enum_Sex
+    {
+        男 = 1,
+        女
+    }
+
+    public enum Enum_User
+    {
+        管理员 = 1,
+        普通会员
+    }
+    /// <summary>
+    /// 计划状态
+    /// </summary>
+    public enum Enum_PlanStatus
+    {
+        未开启 = 0,
+        开始,
+        结束
+    }
+
+    public enum Enum_TradeType
+    {
+        无偿交易 = 1,
+        有偿交易
+    }
+
+    public enum Enum_TradeStatus
+    {
+        进行中 = 1,
+        交易中,
+        已提交,
+        已完成,
+        已关闭
+    }
+
+    public enum Enum_CollectionType
+    {
+        博客 = 1,
+        计划,
+        图书,
+        轮播,
+        音乐,
+        导航,
+        案例,
+        公告
+    }
+
+    public enum Enum_PhotoType
+    {
+        博客类型 = 1,
+        头像,
+        富文本
+    }
+
+    public enum Enum_Blog_Source
+    {
+        原创 = 1,
+        转载,
+        翻译
+    }
+
+    public enum Enum_Email_Type
+    {
+        绑定邮箱 = 1,
+        忘记密码
+    }
+
+    public enum Enum_Education_Type
+    {
+        大专 = 1,
+        本科,
+        硕士,
+        博士,
+        其它
+    }
+
+    public enum Enum_WorkTime_Type
+    {
+        一年 = 1,
+        二年,
+        三年,
+        四年,
+        五年,
+        六年,
+        七年,
+        八年,
+        九年,
+        十年,
+        十年以上
+    }
+
+    public enum Enum_Member_Status
+    {
+        正常 = 0,
+        限制,
+        黑名单,
+        删除
+    }
+}
